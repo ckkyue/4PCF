295e7eb91cf6a5d83d33a41ab5fd479945bc3e3b
Kelvin Yue <ckkyue@connect.ust.hk>
2023-11-07 14:22:51 +0800

    In progress

--- Tetrahedron_generator.py
+++ Tetrahedron_generator.py
@@ -2,38 +2,51 @@ import numpy as np
 import random
 
 def create_single_tetrahedron(position, parity, r, deviation):
-    transform_matrix = np.array([[1, 0, 0],
-                                 [0, np.cos(deviation[0]), np.sin(deviation[0])],
-                                 [0, -np.sin(deviation[0]), np.cos(deviation[0])]]) @ \
-                       np.array([[np.cos(deviation[1]), 0, np.sin(deviation[1])],
-                                 [0, 1, 0],
-                                 [-np.sin(deviation[1]), 0, np.cos(deviation[1])]]) @ \
-                       np.array([[np.cos(deviation[2]), np.sin(deviation[2]), 0],
-                                 [-np.sin(deviation[2]), np.cos(deviation[2]), 0],
-                                 [0, 0, 1]])
+    # position: 3D array
 
-    r_before_trans = np.array([parity * (r[0] + deviation[3]), r[1] + deviation[4], r[2] + deviation[5]])
-
+    r1 = r[0]
+    r2 = r[1]
+    r3 = r[2]
+    delta_theta1 = deviation[0]
+    delta_theta2 = deviation[1]
+    delta_theta3 = deviation[2]
+    delta_r1 = deviation[3]
+    delta_r2 = deviation[4]
+    delta_r3 = deviation[5]
+    
     primary = position
-    secondary_1 = primary + transform_matrix @ np.array([r_before_trans[0], 0, 0])
-    secondary_2 = primary + transform_matrix @ np.array([0, r_before_trans[1], 0])
-    secondary_3 = primary + transform_matrix @ np.array([0, 0, r_before_trans[2]])
 
-    return np.vstack((primary, secondary_1, secondary_2, secondary_3))
+    transform = np.array([[1, 0, 0],
+                          [0, np.cos(delta_theta1), np.sin(delta_theta1)],
+                          [0, -np.sin(delta_theta1), np.cos(delta_theta1)]]) @ \
+                        np.array([[np.cos(delta_theta2), 0, np.sin(delta_theta2)],
+                                  [0, 1, 0],
+                                  [-np.sin(delta_theta2), 0, np.cos(delta_theta2)]]) @ \
+                        np.array([[np.cos(delta_theta3), np.sin(delta_theta3), 0],
+                                  [-np.sin(delta_theta3), np.cos(delta_theta3), 0],
+                                  [0, 0, 1]])
+
+    r_initial = np.array([parity * (r1 + delta_r1), r2 + delta_r2, r3 + delta_r3])
+
+    secondary1 = primary + transform @ np.array([r_initial[0], 0, 0])
+    secondary2 = primary + transform @ np.array([0, r_initial[1], 0])
+    secondary3 = primary + transform @ np.array([0, 0, r_initial[2]])
+
+    return np.vstack((primary, secondary1, secondary2, secondary3))
 
 
 def generate_random_deviations(r_min, r_max):
-    return np.array([random.uniform(-np.pi, np.pi) for _ in range(3)] +
+    return np.array([random.uniform(-np.pi, np.pi) for i in range(3)] +
                     [random.uniform(r_min[i], r_max[i]) for i in range(3)])
 
 
 def create_multiple_tetrahedra(vertices, parity, r, deviation_range):
-    num_of_vertices = vertices.shape[0]
+    num_vert = vertices.shape[0]
     deviation_min, deviation_max = deviation_range[:3], deviation_range[3:]
 
     multiple_tetrahedra = []
 
-    for i in range(num_of_vertices):
+    for i in range(num_vert):
         deviation = generate_random_deviations(deviation_min, deviation_max)
         single_tetrahedron = create_single_tetrahedron(vertices[i], parity, r, deviation)
         multiple_tetrahedra.append(single_tetrahedron)
